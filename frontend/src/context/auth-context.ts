04734e7826cad4a4850d96eec31c5d3c5efa26e8
nivgin <niv1@hiker.org.il>
2026-03-11 19:30:06 +0200

    removed user details from login, adjusted auth context

--- frontend/src/context/auth-context.ts
+++ frontend/src/context/auth-context.ts
@@ -1,9 +1,8 @@
 import { createContext } from "react";
-import type { User } from "../types/User";
 import type { LoginData, RegisterData } from "../types/Auth";
 
 export interface AuthContextType {
-  user: User | null;
+  user: string | null;
   login: (data: LoginData) => Promise<void>;
   register: (data: RegisterData) => Promise<void>;
   logout: () => void;
