0f62255bbee2b7b50a47d2f66a7cce66eebaace9
ShaiLeonBenShimol <shaibs2002@gmail.com>
2026-04-17 18:39:28 +0300

    fixed some typerscript issues

--- frontend/src/components/ProfileForm/ProfileForm.tsx
+++ frontend/src/components/ProfileForm/ProfileForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, FormHelperText, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import type { Control, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
