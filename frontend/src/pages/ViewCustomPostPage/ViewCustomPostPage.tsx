75f296cbe8f0d4c4d10d111165c1172d04691ffe
ShaiLeonBenShimol <shaibs2002@gmail.com>
2026-04-19 20:26:48 +0300

    fixed customPost page

--- frontend/src/pages/ViewCustomPostPage/ViewCustomPostPage.tsx
+++ frontend/src/pages/ViewCustomPostPage/ViewCustomPostPage.tsx
@@ -1,8 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { Box } from "@mui/material";
-import InstructionList from "../../components/InstructionList/InstructionList";
-import IngredientList from "../../components/IngredientList/IngredientList";
 import RecipeTitle from "../../components/RecipeTitle/RecipeTitle";
 import type { PostFull } from "../../types/Post";
 import type { User } from "../../types/User";
@@ -23,11 +21,6 @@ export default function ViewCustomPostPage() {
   return (
     <Box sx={{ backgroundColor: "#f9f7f5", minHeight: "100vh" }}>
       <RecipeTitle post={post} sender={sender} />
-
-      <Box sx={{ maxWidth: "lg", mx: "auto", px: 2, py: 6 }}>
-        <IngredientList ingredients={post.ingredients} />
-        <InstructionList instructions={post.instructions} />
-      </Box>
     </Box>
   );
 }
